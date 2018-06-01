c4aa731f9ac64faaedd183ea3bfbaab2d7ed6e9b
Leo Arias <yo@elopio.net>
2018-06-01 04:06:39 +0000

    Remove the arg-overflow rule, with tests

---
+++ test/solium/no-arg-overlow-rule.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let fs = require('fs');
+let dedent = require("dedent");
+let Solium = require("solium");
+
+let userConfig = JSON.parse(fs.readFileSync('.soliumrc.json', 'utf8'));
+
+describe("[RULE] arg-overflow", function() {
+
+  afterEach(function(done) {
+    Solium.reset();
+    done();
+  });
+
+  it("should ignore more than 3 arguments in the same line", function(done) {
+    let code = dedent`
+                pragma solidity ^0.4.3;
+
+
+                contract Dummy {
+                  function testFunc(uint arg1, uint arg2, uint arg3, uint arg4) public {
+                  }
+                }`,
+        errors = Solium.lint(code, userConfig);
+
+    errors.should.deepEqual([]);
+
+    done();
+  });
+
+});
